e3dc1c44134c78c15f320269f5918256fe526826
Victor Reijgwart <victor_reijgwart@hotmail.com>
2023-08-05 16:22:50 +0200

    Make it possible to load and save traces to host home directory

--- gui/run_gui_docker.sh
+++ gui/run_gui_docker.sh
@@ -10,11 +10,13 @@ if docker info | grep "Runtimes.*nvidia.*" &>/dev/null; then
     --runtime=nvidia --gpus all \
     -e NVIDIA_VISIBLE_DEVICES=all \
     -e NVIDIA_DRIVER_CAPABILITIES=graphics \
+    -v "${HOME}":/home/tracy:rw \
     tracy-gui "$@"
 else
   docker run --rm -it --net=host \
     -e DISPLAY="${DISPLAY}" \
     -e QT_X11_NO_MITSHM=1 \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v "${HOME}":/home/tracy:rw \
     tracy-gui "$@"
 fi
